1d28cf6da58a34ad37408f7cc60b886f2316fcfa
Pradosh Kharel <pradosh.kharel@gmail.com>
2025-01-10 12:05:11 -0600

    Add timer

--- global_entry_notifier.py
+++ global_entry_notifier.py
@@ -3,14 +3,16 @@ import discord_webhook
 import json
 import logging
 import requests
+import sched
+import time
 import yaml
+
 
 class GlobalEntryNotifier:
   def __init__(self, webhook, locations, test):
     self.webhook = webhook
     self.locations = locations
     self.test = test
-
 
   def send_notification(self, message):
       logging.info(f"Sending notification: {message}")
@@ -26,11 +28,11 @@ class GlobalEntryNotifier:
     return slots
 
   def check_locations(self):
-    if self.test:
-      self.send_notification("Test Notification")
-      return
-
     for location in self.locations:
+      # Send out test notification
+      if self.test:
+        self.send_notification(f"Test Notification for {location}")
+        continue
       try:
         slots = self.check_location(location)
       except requests.exceptions.HTTPError as err:
@@ -39,27 +41,48 @@ class GlobalEntryNotifier:
         self.send_notification(msg)
         self.send_notification(json.dumps(err.response.json(), indent=4))
         continue
-      #self.send_notification(f"No appointments found for {location}")
-      if len(slots) != 0:
-        self.send_notification(f"Appointments found for {location}!")
+      if len(slots) == 0:
+        logging.info(f"No appointments found for {location}")
+      else:
+        msg = f"Appointments found for {location}!"
+        logging.info(msg)
+        self.send_notification(msg)
         self.send_notification(json.dumps(slots, indent=4))
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('config', default="config.yaml", help="Config file")
 parser.add_argument('-d', '--debug', action='store_true', help="Debug mode")
+parser.add_argument('-f', '--file', help = "Store log in file")
 parser.add_argument('-t', '--test', action='store_true', help="Test mode to send notification even if no appointments")
 
 args = parser.parse_args()
 
-if args.debug:
-  logging.basicConfig(level=logging.DEBUG)
-else:
-  logging.basicConfig(level=logging.INFO)
+# Set up better formatting for logger
+FORMAT="%(asctime)s %(levelname)-8s %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT, handlers=[
+        logging.FileHandler("global_entry_notifier.log"),
+        logging.StreamHandler()
+    ]
+)
 
 # Parse config file
 with open(args.config, 'r') as stream:
   config_data = yaml.safe_load(stream)
 
 notifier = GlobalEntryNotifier(config_data["webhook"], config_data["locations"], args.test)
-notifier.check_locations()
+
+timer = config_data.get("timer")
+
+# Function to check locations every timer seconds
+def run_in_loop(scheduler, timer, notifier):
+  scheduler.enter(timer, 1, run_in_loop, (scheduler, timer, notifier))
+  notifier.check_locations()
+
+if timer:
+  logging.info(f"Will run every {timer} seconds")
+  scheduler = sched.scheduler(time.time, time.sleep)
+  scheduler.enter(0, 1, run_in_loop, (scheduler, timer, notifier))
+  scheduler.run()
+else:
+  notifier.check_locations()
