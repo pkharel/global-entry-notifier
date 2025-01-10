75ad53e6e0cf1bbbb74f58fd00edf70d4068307a
Pradosh Kharel <pradosh.kharel@gmail.com>
2025-01-10 11:23:15 -0600

    Minor improvements

--- global_entry_notifier.py
+++ global_entry_notifier.py
@@ -5,6 +5,45 @@ import logging
 import requests
 import yaml
 
+class GlobalEntryNotifier:
+  def __init__(self, webhook, locations, test):
+    self.webhook = webhook
+    self.locations = locations
+    self.test = test
+
+
+  def send_notification(self, message):
+      logging.info(f"Sending notification: {message}")
+      webhook = discord_webhook.DiscordWebhook(url=self.webhook, content=message)
+      webhook.execute()
+
+  def check_location(self, location):
+    logging.info(f"Location: {location}")
+    r = requests.get(f"https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId={location}&minimum=1")
+    #r = requests.get(f"https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId=sadlfkjaslas&minimum=1")
+    r.raise_for_status()
+    slots = r.json()
+    return slots
+
+  def check_locations(self):
+    if self.test:
+      self.send_notification("Test Notification")
+      return
+
+    for location in self.locations:
+      try:
+        slots = self.check_location(location)
+      except requests.exceptions.HTTPError as err:
+        msg = f"HTTP error occurred"
+        logging.error(msg)
+        self.send_notification(msg)
+        self.send_notification(json.dumps(err.response.json(), indent=4))
+        continue
+      #self.send_notification(f"No appointments found for {location}")
+      if len(slots) != 0:
+        self.send_notification(f"Appointments found for {location}!")
+        self.send_notification(json.dumps(slots, indent=4))
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('config', default="config.yaml", help="Config file")
@@ -13,34 +52,14 @@ parser.add_argument('-t', '--test', action='store_true', help="Test mode to send
 
 args = parser.parse_args()
 
-# Read YAML file
-with open(args.config, 'r') as stream:
-  config_data = yaml.safe_load(stream)
-
-test = args.test
-
-webhook_url = config_data["webhook"]
-
 if args.debug:
   logging.basicConfig(level=logging.DEBUG)
 else:
   logging.basicConfig(level=logging.INFO)
 
-for location in config_data["locations"]:
-  logging.info(f"Location: {location}")
-  appointments_found = False
-  r = requests.get(f"https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=1&locationId={location}&minimum=1")
-  slots = r.json()
-  if len(slots) == 0:
-    message = f"No appointments available for: {location}"
-  else:
-    message = f"Appointments found for: {location}!\n"
-    message += json.dumps(slots, indent=4)
-    appointments_found = True
+# Parse config file
+with open(args.config, 'r') as stream:
+  config_data = yaml.safe_load(stream)
 
-  logging.info(message)
-
-  if test or appointments_found:
-    logging.info("Sending notification")
-    webhook = discord_webhook.DiscordWebhook(url=webhook_url, content=message)
-    webhook.execute()
+notifier = GlobalEntryNotifier(config_data["webhook"], config_data["locations"], args.test)
+notifier.check_locations()
