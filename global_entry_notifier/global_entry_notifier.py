e2dcfb0824a5ed07731cd8a32980f9429026bda2
Pradosh Kharel <pradosh.kharel@gmail.com>
2025-01-10 14:36:38 -0600

    Add support for twilio voice call

--- global_entry_notifier/global_entry_notifier.py
+++ global_entry_notifier/global_entry_notifier.py
@@ -4,15 +4,33 @@ import json
 import logging
 import requests
 import sched
+import twilio
 import time
 import yaml
 
+from twilio.rest import Client
 
 class GlobalEntryNotifier:
-    def __init__(self, webhook, locations, test):
+    def __init__(self, webhook, twilio, locations, test):
+        self.twilio = twilio
         self.webhook = webhook
         self.locations = locations
         self.test = test
+
+    def send_voice_call(self):
+        # Download the helper library from https://www.twilio.com/docs/python/install
+        # Find your Account SID and Auth Token at twilio.com/console
+        # and set the environment variables. See http://twil.io/secure
+        account_sid = self.twilio["account_sid"]
+        studio_flow = self.twilio["studio_flow"]
+        auth_token = self.twilio["auth_token"]
+        from_num   = self.twilio["from"]
+        to_num   = self.twilio["to"]
+        client = Client(account_sid, auth_token)
+        execution = client.studio.v2.flows(
+            studio_flow
+        ).executions.create(to=to_num, from_=from_num)
+        logging.info(execution.sid)
 
     def send_notification(self, message):
         logging.info(f"Sending notification: {message}")
@@ -29,6 +47,7 @@ class GlobalEntryNotifier:
         return slots
 
     def check_locations(self):
+        found_appointment = False
         for location in self.locations:
             # Send out test notification
             if self.test:
@@ -49,6 +68,9 @@ class GlobalEntryNotifier:
                 logging.info(msg)
                 self.send_notification(msg)
                 self.send_notification(json.dumps(slots, indent=4))
+                found_appointment = True
+        if found_appointment:
+            self.send_voice_call()
 
 
 parser = argparse.ArgumentParser()
@@ -80,8 +102,10 @@ logging.basicConfig(
 with open(args.config, "r") as stream:
     config_data = yaml.safe_load(stream)
 
+twilio_config = config_data.get("twilio")
+
 notifier = GlobalEntryNotifier(
-    config_data["webhook"], config_data["locations"], args.test
+    config_data["webhook"], twilio_config, config_data["locations"], args.test
 )
 
 timer = config_data.get("timer")
